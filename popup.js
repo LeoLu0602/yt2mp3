e3292c2384105c9e65b2d57c4766af764c4bc41f
Leo <96434243+LeoLu0602@users.noreply.github.com>
2023-07-11 01:34:27 +0800

    all done

---
+++ popup.js
@@ -0,0 +1,49 @@
+const checkLink = (link) => {
+    const pattern = /youtube.com\/watch/;
+    return pattern.test(link);
+};
+
+const convert = (videoId) => {
+    console.log('[yt2mp3] Video ID: ' + videoId);
+
+    fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '912a946990mshd4f4138344b2b9bp187843jsn8000b12a1fdb',
+            'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log('[yt2mp3] Data: ' + data);
+        window.open(data.link, '_blank');
+    })
+    .catch(error => {
+        console.log('[yt2mp3] Error: ' + error);
+    });
+};
+
+const downloadVideo = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        link = tabs[0].url;
+
+        const isLinkValid = checkLink(link);
+
+        if (isLinkValid === false) {
+            console.log('[yt2mp3] Invalid Link');
+            return;
+        }
+
+        console.log('[yt2mp3] Downloading...');
+
+        const startIndex = link.indexOf('?') + 3;
+        const endIndex = link.indexOf('&');
+        const videoId = link.slice(startIndex, endIndex);
+        
+        convert(videoId);
+    });
+};
+
+document.querySelector('button').addEventListener('click', downloadVideo);
---
+++ script.js
@@ -0,0 +1,1 @@
+console.log('[yt2mp3] Working...');
